d29900b8576d25b6a453e31ed572830b893dd1a4
hubert.wesolowski <hubert.wesolowski@callstack.com>
2024-03-07 10:11:39 +0100

    refactor: Move App contexts to separate file

--- App.tsx
+++ App.tsx
@@ -1,47 +1,16 @@
-import NetInfo from '@react-native-community/netinfo';
-import {
-  onlineManager,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { TemperatureUnitsContextProvider } from './src/contexts/temperature-units-context';
+import { AppContexts } from './src/contexts/app-contexts';
 import { Routes } from './src/routes/routes';
-import { defaultTheme } from './src/theme';
-
-const queryClient = new QueryClient();
-
-onlineManager.setEventListener(setOnline => {
-  return NetInfo.addEventListener(state => {
-    setOnline(!!state.isConnected);
-  });
-});
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <AppContexts>
       <StatusBar barStyle="light-content" />
       <Routes />
-    </>
+    </AppContexts>
   );
 }
 
-function AppContext(): React.JSX.Element {
-  return (
-    <SafeAreaProvider>
-      <PaperProvider theme={defaultTheme}>
-        <QueryClientProvider client={queryClient}>
-          <TemperatureUnitsContextProvider>
-            <App />
-          </TemperatureUnitsContextProvider>
-        </QueryClientProvider>
-      </PaperProvider>
-    </SafeAreaProvider>
-  );
-}
-
-export default AppContext;
+export default App;
